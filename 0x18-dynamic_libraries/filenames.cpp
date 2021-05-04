8e7736e013830bbe28f4ff09b6d3c962b43f3901
JRodriguez9510 <jrodriguez9510@outlook.com>
2021-05-03 22:20:18 -0500

    Add Ecercises

---
+++ 0x18-dynamic_libraries/filenames.cpp
@@ -0,0 +1,22 @@
+_putchar.c _isalpha.c _abs.c _isupper.c _isdigit.c _strlen.c _puts.c _strcpy.c _atoi.c _strcat.c _strncat.c _strncpy.c _strcmp.c _memset.c _memcpy.c _strchr.c _strspn.c _strpbrk.c _strstr.c _islower.c
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
